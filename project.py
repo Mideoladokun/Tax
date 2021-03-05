d3c32fde3cdd1e0e4e9f3ffc0f1d75c2a76c1c00
Mideoladokun <mideoladokun@gmail.com>
2021-03-05 13:21:01 +0100

    https://github.com/Mideoladokun/tax.git

--- project.py
+++ project.py
@@ -1,9 +1,12 @@
 """salary = int (input("enter annual salary"))"""
-def tax_relief(salary = int (input("enter annual salary"))):
+def tax_relief(salary):
     if salary > 200000:
         return((0.01*salary) + (0.2*salary))
     else:
-        return(200000 + (0.2*salary))
-print(tax_relief)
+        return (200000 + (0.2*salary))
+salary = int (input("enter annual salary"))
+result = tax_relief(salary)
+print(result)
+
 """gross_salary = input("enter annual salary ")
 print(int(gross_salary + 50000))"""
